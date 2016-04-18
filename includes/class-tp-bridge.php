58c4c93ee30a52839ed57c83555788838616e732
Sarin Achawaranont <sarin_a@cleverse.com>
2016-04-18 16:57:08 +0700

    add sample admin setting page

--- includes/class-tp-bridge.php
+++ includes/class-tp-bridge.php
@@ -160,7 +160,9 @@ class Tp_Bridge {
 		// Add Settings link to the plugin
 		$plugin_basename = plugin_basename( plugin_dir_path( __DIR__ ) . $this->plugin_name . '.php' );
 		$this->loader->add_filter( 'plugin_action_links_' . $plugin_basename, $plugin_admin, 'add_action_links' );
-
+		
+		// Save/Update our plugin options
+		$this->loader->add_action('admin_init', $plugin_admin, 'options_update');
 
 	}
 
